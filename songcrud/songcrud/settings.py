92f08d173db07cbd28dc81ba45d38784b949c6e7
Gudnuel <obadakiemmanuel@gmail.com>
2022-10-28 02:32:12 +0100

    adding the pip django_enviro dependence to Week5 task

--- songcrud/songcrud/settings.py
+++ songcrud/songcrud/settings.py
@@ -11,19 +11,28 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import environ
+import os
 
+
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wcw@p=%quoz*ahz&xw^3s@r2o25*#p8*--_&uzdct8#k1hv%-+'
+# SECRET_KEY = 'django-insecure-wcw@p=%quoz*ahz&xw^3s@r2o25*#p8*--_&uzdct8#k1hv%-+'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 
